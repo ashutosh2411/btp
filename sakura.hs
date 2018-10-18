549bf30cf4db85d20f84e35e1d4ce6d095f2eb7b
okabe <ashutoshu2411@gmail.com>
2018-10-18 11:10:23 +0530

    Runs tests on bloc.

--- sakura.hs
+++ sakura.hs
@@ -6,21 +6,23 @@ type HashF = [Word8] -> [Word8]
 
 data HShape =
   InnerHash HShape
-  |Concat [HShape]  
-  |Interleaving [HShape]  
-  |Slice Int Int  
+  |Concat [HShape]
+  |Interleaving [HShape]
+  |Slice Int Int
   |Pad  BStr  deriving Show
 
 my_slice :: Int -> Int -> BStr -> BStr
 my_slice from to = (drop from).(take to)
 
 s :: HashF -> HShape -> BStr -> BStr
+-- Serial hash computation
 s h (InnerHash aShape) bStr  =  h (s h aShape bStr)
 s h (Concat l) bStr = concat  (map (\x-> s h x bStr)  l)
 s _ (Slice from to) bStr = my_slice from to bStr
 s _ (Pad x) _ = x
 
 p :: HashF -> HShape -> BStr -> BStr
+-- Parallel hash computation
 p h (InnerHash aShape) bStr = h $ p h aShape  bStr
 p h (Concat l) bStr = concat (parMap rpar (\mu -> p h mu bStr) l)
 p _ (Slice from to) bStr = my_slice from to bStr
